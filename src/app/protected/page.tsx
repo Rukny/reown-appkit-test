6a3507fa293cebef5d8a68d41c3a43033f37823a
rkghostivy <rukny.kawar@ghost-ivy.com>
2024-10-20 11:00:43 +0300

    redirect to home

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,9 +1,18 @@
 "use client"
 import { disconnect } from 'wagmi/actions';
 import { config } from '../providers';
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function ProtectedPage() {
+    const router = useRouter();
+    const {status, data: session} = useSession();
+    useEffect(() => {
+        console.log(session)
+        if(!session)
+            router.push("/");
+      }, []);
   return (
     <button                 onClick={async () => {
         await disconnect(config);
